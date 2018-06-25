1481b269013b3c8c43cbb4da55013bbb7ed672eb
BrilZliaN <brilzlian@ya.ru>
2018-06-25 21:10:04 +0300

    Extended test dataset

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,5 @@
 INSERT INTO `applications` (`CONTACT_ID`, `DT_CREATED`, `PRODUCT_NAME`) VALUES ('2', '2018-06-25 18:33:10', 'credit_card');
-INSERT INTO `applications` (`CONTACT_ID`, `DT_CREATED`, `PRODUCT_NAME`) VALUES ('3', '2018-06-25 18:34:40', 'debit_card');
+INSERT INTO `applications` (`CONTACT_ID`, `DT_CREATED`, `PRODUCT_NAME`) VALUES ('1', '2018-06-25 18:34:40', 'debit_card');
+INSERT INTO `applications` (`CONTACT_ID`, `DT_CREATED`, `PRODUCT_NAME`) VALUES ('3', '2018-06-25 18:36:15', 'debit_card');
+INSERT INTO `applications` (`CONTACT_ID`, `DT_CREATED`, `PRODUCT_NAME`) VALUES ('3', '2018-06-25 18:39:50', 'credit_card');
+INSERT INTO `applications` (`CONTACT_ID`, `DT_CREATED`, `PRODUCT_NAME`) VALUES ('2', '2018-06-25 21:00:29', 'debit_card');
