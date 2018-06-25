258fe114d0e9a5d84afcad0af9ff26c82bfe7f36
BrilZliaN <brilzlian@ya.ru>
2018-06-25 21:09:18 +0300

    SQL bugfix, applications are now really requested by contact id

--- src/main/java/me/BrilZliaN/TinkoffTestTask/db/ApplicationServiceImpl.java
+++ src/main/java/me/BrilZliaN/TinkoffTestTask/db/ApplicationServiceImpl.java
@@ -10,38 +10,38 @@ import me.BrilZliaN.TinkoffTestTask.Application;
 
 @Service
 public class ApplicationServiceImpl implements ApplicationService {
-	
+
 	private final ApplicationRepository applicationRepository;
-	
-    @Autowired
-    public ApplicationServiceImpl(ApplicationRepository applicationRepository) {
-        this.applicationRepository = applicationRepository;
-    }
+
+	@Autowired
+	public ApplicationServiceImpl(ApplicationRepository applicationRepository) {
+		this.applicationRepository = applicationRepository;
+	}
 
 	@Override
 	public Optional<Application> getApplicationById(Integer applicationId) {
 		return Optional.ofNullable(applicationRepository.selectApplicationById(applicationId));
 	}
-	
+
 	@Override
 	public Optional<Application> getApplicationByContactId(Integer contactId) {
-		return Optional.ofNullable(applicationRepository.selectApplicationById(contactId));
+		return Optional.ofNullable(applicationRepository.selectApplicationByContactId(contactId));
 	}
-	
+
 	@Override
-    @Transactional
+	@Transactional
 	public boolean addApplication(Application application) {
 		return applicationRepository.insertApplication(application) > 0;
 	}
 
 	@Override
-    @Transactional
+	@Transactional
 	public boolean updateApplicationById(Application application) {
 		return applicationRepository.updateApplicationById(application) > 0;
 	}
 
 	@Override
-    @Transactional
+	@Transactional
 	public boolean deleteApplicationById(Application application) {
 		return applicationRepository.deleteApplicationById(application) > 0;
 	}
